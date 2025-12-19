22b45aa330ef773ffae79b44681641956482ea9e
seansim-kor <seansim.kor@gmail.com>
2025-12-20 01:15:39 +0800

    Add responsive classes to handwriting.jpeg for centered background positioning

--- components/BrandStory.tsx
+++ components/BrandStory.tsx
@@ -7,8 +7,7 @@ const BrandStory: React.FC = () => {
       {/* Background Image & Overlay */}
       <div className="absolute inset-0 z-0">
         <img 
-src="/handwriting.jpeg"          
-          />
+src="/handwriting.jpeg"        className="w-full h-full object-cover object-center" />         />
         <div className="absolute inset-0 bg-white/90 backdrop-blur-[2px]"></div>
       </div>
       
